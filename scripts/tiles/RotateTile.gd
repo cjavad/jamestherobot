b1f4d9fadd952566d1ee65204882a915aff7460a
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2021-08-07 13:25:36 +0200

    turn tile

--- scripts/tiles/RotateTile.gd
+++ scripts/tiles/RotateTile.gd
@@ -2,6 +2,12 @@ extends Tile
 
 export var left: bool = true;
 
+func _process(delta):
+	if self.left:
+		$cog.rotate(Vector3.UP, delta * -30.0);
+	else:
+		$cog.rotate(Vector3.UP, delta * 30.0);
+
 func update_agent(agent: Agent) -> void:
 	if self.left:
 		agent.turn_left()
